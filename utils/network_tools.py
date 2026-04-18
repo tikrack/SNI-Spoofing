b77789113bfbb2660d76f893f9ac4abaa22b2a27
TikRack <137154723+tikrack@users.noreply.github.com>
2026-04-18 17:38:30 +0330

    Update network_tools.py

--- utils/network_tools.py
+++ utils/network_tools.py
@@ -1,25 +1,19 @@
 import socket
 
 
-def get_default_interface_ipv4(addr="8.8.8.8") -> str:
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def _get_interface_ip(family, addr):
     try:
-        s.connect((addr, 53))
-    except OSError:
-        return ""
-    else:
-        return s.getsockname()[0]
-    finally:
-        s.close()
+        with socket.socket(family, socket.SOCK_DGRAM) as s:
+            s.settimeout(1.5)
+            s.connect((addr, 53))
+            return s.getsockname()[0]
+    except Exception:
+        return None
 
 
-def get_default_interface_ipv6(addr="2001:4860:4860::8888") -> str:
-    s = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
-    try:
-        s.connect((addr, 53))
-    except OSError:
-        return ""
-    else:
-        return s.getsockname()[0]
-    finally:
-        s.close()
+def get_default_interface_ipv4(addr="8.8.8.8"):
+    return _get_interface_ip(socket.AF_INET, addr)
+
+
+def get_default_interface_ipv6(addr="2001:4860:4860::8888"):
+    return _get_interface_ip(socket.AF_INET6, addr)
